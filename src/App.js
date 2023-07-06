e17e137bac1f894768217555049589c310464101
Ashish Ranjan <akp9801@gmail.com>
2023-07-06 12:31:48 +0530

    fetched api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react'
-import { Currency } from './components/Currency/Currency'
+import React, { useEffect } from 'react';
+import { Currency } from './components/Currency/Currency';
+
+const Base_URL = 'https://api.exchangerate.host/latest';
 
 export default function App() {
+
+  useEffect(() => {
+    (async () => {
+      await fetch(Base_URL)
+      .then(res => res.json())
+      .then(data => console.log(data))
+    })();
+  }, [])
+
   return (
     <div>
       <Currency />
